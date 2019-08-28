099a9f600a4a2e8419944c2258ba7f64fca0dabe
roliiii <roli4759@hotmail.com>
2019-08-28 19:32:47 +0200

    syntactic sugar

--- reverseString.py
+++ reverseString.py
@@ -3,9 +3,7 @@ message = list('1234')
 i =  0
 j = len(message) - 1
 while i < j:
-    temp = message[i]
-    message[i] = message[j]
-    message[j] = temp
+    message[i], message[j] = message[j], message[i]
     i = i + 1
     j = j - 1 
 
