af62a1dcc1b0c74a70899e945547987d60ed3006
roliiii <roli4759@hotmail.com>
2019-09-01 23:03:59 +0200

    refactor

--- smallExcercises/sumOfTwoLinkedList.py
+++ smallExcercises/sumOfTwoLinkedList.py
@@ -1,28 +1,32 @@
 # Definition for singly-linked list.
 class ListNode(object):
-  def __init__(self, x):
-    self.val = x
-    self.next = None
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution:
-  def addTwoNumbers(self, l1, l2, c = 0):
-    if(l1 is None and l2 is None):
-      if(c!=0):
-        return ListNode(c)
-      return None
+    def addTwoNumbers(self, l1, l2, c=0):
+        if l1 is None and l2 is None:
+            if c != 0:
+                return ListNode(c)
+            return None
 
-    if l1 is not None and l2 is not None:
-      newNode = ListNode((l1.val + l2.val + c)%10)
-      newNode.next = self.addTwoNumbers( l1.next,l2.next, (l1.val+l2.val+c)//10)
-      return newNode
-    elif l1 is not None:
-      newNode = ListNode((l1.val + c)%10)
-      newNode.next = self.addTwoNumbers(l1.next,None, (l1.val+c)//10)
-      return newNode
-    elif l2 is not None:
-      newNode = ListNode((l2.val + c)%10)
-      newNode.next = self.addTwoNumbers(None, l2.next, (l2.val+c)//10)
-      return newNode
+        a, b = 0, 0
+
+        if l1:
+            a = l1.val
+        if l2:
+            b = l2.val
+
+        sum = a + b + c
+
+        newNode = ListNode(sum % 10)
+        newNode.next = self.addTwoNumbers(
+            l1.next if l1 else None, l2.next if l2 else None, sum // 10
+        )
+        return newNode
+
 
 l1 = ListNode(9)
 l1.next = ListNode(4)
@@ -35,6 +39,6 @@ l2.next.next.next = ListNode(9)
 
 result = Solution().addTwoNumbers(l1, l2)
 while result:
-  print(result.val),
-  result = result.next
+    print(result.val),
+    result = result.next
 # 41101
