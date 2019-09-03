8acfaea969fb47f422c63f4ff29feccb0c0f3465
roliiii <roli4759@hotmail.com>
2019-09-03 20:46:15 +0200

    challenges

--- pythonChallange/4.py
+++ pythonChallange/4.py
@@ -1,7 +1,22 @@
 import re
 from urllib.request import urlopen
 
-url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=44827"
-page = urlopen(url)
-data = page.read()
-print(data)
+
+param=12345
+
+while param is not None:
+    url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+str(param)
+    page = urlopen(url)
+    data = page.read().decode("utf-8")
+
+    searcRes = re.search(r"""(?<=and the next nothing is )(\d)+""",data)
+
+    if(data=='Yes. Divide by two and keep going.'):
+        divider=2
+        param=int(param)//2
+    elif searcRes is None:
+        param = None
+    else:
+        param = int(searcRes.group());
+    print(data)
+    
