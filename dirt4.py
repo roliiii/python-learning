837a27f8307f9b1785761dc9990dc60e29701ad9
roliiii <roli4759@hotmail.com>
2019-09-26 19:36:23 +0200

    dirt4

--- dirt4.py
+++ dirt4.py
@@ -1,14 +1,42 @@
 import requests
 import json
+from enum import Enum
 
+class EventType(Enum):
+    DiRT_DAILY_LIVE = "DiRT DAILY LIVE"
+    OWNERS_CLUB = "THE OWNERS’ CLUB / DELTA DAILY"
 
-url = r"""https://www.dirt4game.com/api/leaderboard/event/35675/stage/1/page/1?assists=0&controls=0&orderByTotal=true&pageSize=100&platform=0&players=0"""
-data = requests.get(url, cookies={'ASP.NET_SessionId':'tein1ie5zj0kxcptu4rhyu2p'})
-data = data.text
+class Dirt4RankUtil:
+    def __init__(self, gamerMode: bool):
+        self.session = requests.Session() 
+        self.session.put(r"""https://www.dirt4game.com/api/profile/set-handling""",{"isAltHandling":gamerMode})
 
-ranks = json.loads(data)
+    def getUrl(self, eventId, page = 1):
+        return r"""https://www.dirt4game.com/api/leaderboard/event/"""+str(eventId)+"""/stage/1/page/"""+str(page)+r"""?assists=0&controls=0&orderByTotal=true&pageSize=100&platform=0&players=0"""
+    
+    def getEventData(self, eventId, page = 1):
+        data = self.session.get(self.getUrl(eventId, page))
+        return data.json()
 
-for item in ranks["Entries"]:
-    if item["Name"] == "Fluffy":
-        print("Rank: " + str(item["Rank"]) + "/" + str(len(ranks["Entries"])))
-        break
+    def getEventId(self, eventType: EventType):
+        data = self.session.get(r"""https://www.dirt4game.com/api/event/summary/?altHandling=false""")
+        respJson = data.json()
+        actEventTypeData = [x for x in respJson["eventTypes"] if x["name"] == eventType.value]
+
+        return actEventTypeData[0]["events"][0]["eventId"]
+
+    def getRank(self, eventType: EventType, userName: str):
+        eventId = self.getEventId(eventType)
+        parsedData = self.getEventData(eventId)
+
+        for i in range(1,parsedData["PageCount"] + 1):
+            if(i != 1):
+                parsedData = getEventData(session, eventId, i)
+            for item in parsedData["Entries"]:
+                if item["Name"] == userName:
+                    return("Rank: " + str(item["Rank"]) + "/" + str(len(parsedData["Entries"])))
+            return None
+
+if __name__ == "__main__":
+    dirt = Dirt4RankUtil(False)
+    print(dirt.getRank(EventType.OWNERS_CLUB,"Fluffy"))
