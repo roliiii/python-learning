fb9597afc9e70505f48721d06a326d3ffeed887d
roliiii <roli4759@hotmail.com>
2019-09-26 19:38:52 +0200

    dirt4 change method visibility

--- dirt4.py
+++ dirt4.py
@@ -11,14 +11,14 @@ class Dirt4RankUtil:
         self.session = requests.Session() 
         self.session.put(r"""https://www.dirt4game.com/api/profile/set-handling""",{"isAltHandling":gamerMode})
 
-    def getUrl(self, eventId, page = 1):
+    def __getUrl(self, eventId, page = 1):
         return r"""https://www.dirt4game.com/api/leaderboard/event/"""+str(eventId)+"""/stage/1/page/"""+str(page)+r"""?assists=0&controls=0&orderByTotal=true&pageSize=100&platform=0&players=0"""
     
-    def getEventData(self, eventId, page = 1):
-        data = self.session.get(self.getUrl(eventId, page))
+    def __getEventData(self, eventId, page = 1):
+        data = self.session.get(self.__getUrl(eventId, page))
         return data.json()
 
-    def getEventId(self, eventType: EventType):
+    def __getEventId(self, eventType: EventType):
         data = self.session.get(r"""https://www.dirt4game.com/api/event/summary/?altHandling=false""")
         respJson = data.json()
         actEventTypeData = [x for x in respJson["eventTypes"] if x["name"] == eventType.value]
@@ -26,12 +26,12 @@ class Dirt4RankUtil:
         return actEventTypeData[0]["events"][0]["eventId"]
 
     def getRank(self, eventType: EventType, userName: str):
-        eventId = self.getEventId(eventType)
-        parsedData = self.getEventData(eventId)
+        eventId = self.__getEventId(eventType)
+        parsedData = self.__getEventData(eventId)
 
         for i in range(1,parsedData["PageCount"] + 1):
             if(i != 1):
-                parsedData = getEventData(session, eventId, i)
+                parsedData = __getEventData(session, eventId, i)
             for item in parsedData["Entries"]:
                 if item["Name"] == userName:
                     return("Rank: " + str(item["Rank"]) + "/" + str(len(parsedData["Entries"])))
