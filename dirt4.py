c6296452a7505c7f2caa1dd8cac011e1dbc339c4
roliiii <roli4759@hotmail.com>
2019-09-27 22:22:24 +0200

    fstring

--- dirt4.py
+++ dirt4.py
@@ -10,17 +10,17 @@ class Dirt4RankUtil:
     def __init__(self, gamerMode: bool):
         self.gamerMode = gamerMode
         self.session = requests.Session() 
-        self.session.put(r"""https://www.dirt4game.com/api/profile/set-handling""",{"isAltHandling":gamerMode})
+        self.session.put(r"https://www.dirt4game.com/api/profile/set-handling",{"isAltHandling":gamerMode})
 
     def __getUrl(self, eventId, page = 1):
-        return r"""https://www.dirt4game.com/api/leaderboard/event/"""+str(eventId)+"""/stage/1/page/"""+str(page)+r"""?assists=0&controls=0&orderByTotal=true&pageSize=100&platform=0&players=0"""
+        return f"https://www.dirt4game.com/api/leaderboard/event/{str(eventId)}/stage/1/page/{str(page)}?assists=0&controls=0&orderByTotal=true&pageSize=100&platform=0&players=0"
     
     def __getEventData(self, eventId, page = 1):
         data = self.session.get(self.__getUrl(eventId, page))
         return data.json()
 
     def __getEventId(self, eventType: EventType):
-        data = self.session.get(r"""https://www.dirt4game.com/api/event/summary/?altHandling="""+str(self.gamerMode))
+        data = self.session.get(f"https://www.dirt4game.com/api/event/summary/?altHandling={str(self.gamerMode)}")
         respJson = data.json()
         actEventTypeData = [x for x in respJson["eventTypes"] if x["name"] == eventType.value]
 
@@ -38,7 +38,7 @@ class Dirt4RankUtil:
                 if item["Name"] == userName:
                     rank = str(item["Rank"])
                     break
-            return rank + "/" + str((parsedData["PageCount"]-1)*100+len(self.__getEventData(eventId, parsedData["PageCount"])["Entries"]))
+            return f"{rank}/{str((parsedData['PageCount']-1)*100+len(self.__getEventData(eventId, parsedData['PageCount'])['Entries']))}"
 
 if __name__ == "__main__":
     userName = "Fluffy"
